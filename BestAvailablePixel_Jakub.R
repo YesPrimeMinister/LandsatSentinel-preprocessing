bbe30a71f3649eafd194f489af41d8d30f010b8c
jdvorak <jakub.dvorak@natur.cuni.cz>
2023-03-30 14:33:24 +0200

    Initial commit of R scripts

---
+++ BestAvailablePixel_Jakub.R
@@ -0,0 +1,155 @@
+# =============================================================================
+# GCG - EARTH OBSERVATION
+# SESSION 4: Higher level image features
+# Name, Name, ...
+# =============================================================================
+
+# =============================================================================
+# load required packages
+l_packages <- c("raster", "rgdal", "lubridate")
+for (p in l_packages){
+  if(! p %in% installed.packages()){
+    install.packages(p, dependencies = TRUE)
+  }
+  library(p, character.only = T)
+}
+
+# =============================================================================
+# Part I - Best-Available-Pixel compositing
+# =============================================================================
+
+
+dir_main <- 'e:/data_krkonose/session_03-05'
+getwd()
+setwd(dir_main)
+
+# =============================================================================
+# 1) Prepare input
+
+# create list of desired image band files
+l_bands <- c("BLU", "GRN", "RED", "NIR", "SW1", "SW2", "NDV", "EVI", "TCG", 
+             "TCB", "TCW")
+l_files <- sapply(l_bands, function(x){
+  paste0("imagery/time-series/2014-2016_001-365_HL_TSA_LNDLG_", x, "_TSS.tif")
+})
+print(l_files)
+
+# read raster stacks
+l_stacks <- lapply(l_files, raster::stack)
+
+# retrieve necessary variables for compositing (DOYs and year vectors)
+bandnames <- names(l_stacks$BLU)
+datestring <- lapply(bandnames, function(x) as.Date(substr(x, 2, 9), "%Y%m%d"))
+doys <- as.numeric(lapply(datestring, yday))
+years <- as.numeric(lapply(datestring, year))
+
+# read cloud distance and valid pixels (masks) stacks
+cdists <- raster::stack('imagery/bap/2014-2016_001-365_HL_TSA_LNDLG_CDS_TSS.tif')
+vld <- raster::stack('imagery/bap/2014-2016_001-365_HL_TSA_LNDLG_VLD_TSS.tif')
+
+
+# =============================================================================
+# 2) Parameterization
+
+# path to the bap_compositing-function including .R suffix
+getwd()
+source('functions.R')
+
+# Composite 1
+# parameters
+target_year <- 2015
+target_doy <- 90
+
+w_year <- 0.0
+w_doy <- 0.6
+w_cdist <- 0.4
+
+max_doff <- 30
+max_yoff <- 4
+
+min_cdist <- 100
+max_cdist <- 500
+
+# call bap_score-function for 1st composite
+bapscore1 <- bap_score(doy=doys, year=years, cloud_dist=cdists, 
+                       target_doy=target_doy, target_year=target_year, 
+                       w_doy=w_doy, w_year=w_year, w_cloud_dist=w_cdist, 
+                       max_doy_offset=max_doff, max_year_offset=max_yoff, 
+                       min_cloud_dist=min_cdist, max_cloud_dist=max_cdist,
+                       valid_pixels=vld)
+
+# create composites for each band from index object
+l_composites <- lapply(l_stacks, create_bap, idx_raster=bapscore1$idx)
+
+# create stack of composite rasters + DOY and YEAR info and write to disc
+comp <- raster::brick(c(l_composites, bapscore1$doy, bapscore1$year))
+print(comp)
+
+outname <- paste0('imagery/bap/LNDLG_BAP_Y', toString(target_year), '-', 
+                  toString(max_yoff), '_DOY', toString(target_doy), '-', 
+                  toString(max_doff), '.tif')
+writeRaster(comp, outname, format='GTiff', datatype = 'INT2U', overwrite=T,
+            progress='text')
+
+# Composite 2
+# ...
+
+
+# =============================================================================
+# Part II - Spectral-Temporal-Metrics
+# =============================================================================
+# ..
+
+
+evi <- stack('imagery/time-series/2014-2016_001-365_HL_TSA_LNDLG_EVI_TSS.tif')
+tcb <- stack('imagery/time-series/2014-2016_001-365_HL_TSA_LNDLG_TCB_TSS.tif')
+tcg <- stack('imagery/time-series/2014-2016_001-365_HL_TSA_LNDLG_TCG_TSS.tif')
+
+# retrieve temporal information from bandnames
+library(lubridate)
+datestring <- unlist(lapply(names(tcb), function(x) substr(x, 2, 9)))
+dates <- as.POSIXlt(datestring, format = "%Y%m%d")
+
+# on this date-object, we can perform logical operations
+# for example, let's find the indices for the months June-August only
+condition <- (year(dates) %in% 2014:2016) & (month(dates) %in% 8:11)
+
+# convert raster subset to matrix
+evi_matrix <- as.matrix(evi[[which(condition)]])
+tcg_matrix <- as.matrix(tcg[[which(condition)]])
+tcb_matrix <- as.matrix(tcb[[which(condition)]])
+
+# calculate P10 across rows in matrix
+# evi_p10 <- apply(evi_matrix, 1, FUN=quantile, probs = c(.10), na.rm=T)
+# evi_p10 <- apply(evi_matrix, 1, FUN=sd, na.rm=T)
+evi_p10 <- apply(evi_matrix, 1, FUN=quantile, probs = c(.85), na.rm=T)
+tcg_p10 <- apply(tcg_matrix, 1, FUN=quantile, probs = c(.85), na.rm=T)
+tcb_p10 <- apply(tcb_matrix, 1, FUN=sd, na.rm=T)
+
+# write results to empty raster
+evi_p10_raster <- raster(nrows=evi@nrows, 
+                         ncols=evi@ncols, 
+                         crs=evi@crs, 
+                         vals=evi_p10,
+                         ext=extent(evi))
+
+tcg_p10_raster <- raster(nrows=evi@nrows, 
+                         ncols=evi@ncols, 
+                         crs=evi@crs, 
+                         vals=tcg_p10,
+                         ext=extent(evi))
+
+tcb_p10_raster <- raster(nrows=evi@nrows, 
+                         ncols=evi@ncols, 
+                         crs=evi@crs, 
+                         vals=tcb_p10,
+                         ext=extent(evi))
+
+
+
+outname_stm <- 'tcb_811_sd.tif'
+writeRaster(tcb_p10_raster, outname_stm, format='GTiff', datatype = 'INT2S', overwrite=T,
+            progress='text')
+
+# plot result
+plot(evi_p10_raster)
